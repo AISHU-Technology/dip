cead2b275889182236b5aee696a588c7c4f7e1e8
kyriezyy <zhou.yangyang@aishu.cn>
2025-12-10 09:51:53 +0800

    reset backend

--- hub/backend/src/infrastructure/api/__init__.py
+++
@@ -1,6 +0,0 @@
-"""HTTP API 适配器"""
-from .routes import router
-from .dependencies import get_app_service
-
-__all__ = ["router", "get_app_service"]
-
--- hub/backend/src/infrastructure/external/__init__.py
+++
@@ -1,6 +0,0 @@
-"""外部服务适配器"""
-from .proton_service import ProtonService
-from .package_parser import ZipPackageParser
-
-__all__ = ["ProtonService", "ZipPackageParser"]
-
